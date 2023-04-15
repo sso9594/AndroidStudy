a856edfe689d3c68ddee21f4619fbe10f91bbcec
신승용 <sinseung-yong@sinseung-yong-ui-MacBookAir.local>
2023-04-15 20:07:46 +0900

    update

---
+++ AddNewActivity/app/src/main/java/com/example/addnewactivity/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.addnewactivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnNewActivity= (Button)findViewById(R.id.btnNewActivity);
+        btnNewActivity.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(),
+                        SecondActivity.class);
+            }
+        });
+    }
+}
